75efaef3bb392b59566d529c74139889e08ca8c4
Andrei Tcibin <tsibin.andr@gmail.com>
2020-06-07 17:27:22 +0300

    Add missing pdf extension

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ GITHUB_WORKSPACE="/github/workspace"
 INPUT_TEX="$INPUT_TEX"
 OUTPUT_DIR="outputs"
 OUTPUT_PDF=$(basename "$INPUT_TEX")
-OUTPUT_PDF="${OUTPUT_PDF%.*}"
+OUTPUT_PDF="${OUTPUT_PDF%.*}.pdf"
 
 cd "$GITHUB_WORKSPACE"
 mkdir "$OUTPUT_DIR"
